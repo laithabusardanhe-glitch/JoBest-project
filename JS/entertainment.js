9dd652212c6e3cc7455b52771fed7b6cf1f9ebe1
laithabusardanhe-glitch <laithabusardanhe@gmail.com>
2026-03-27 16:22:21 +0300

    update project

---
+++ JS/entertainment.js
@@ -0,0 +1,24 @@
+const searchInput = document.getElementById("searchInput");
+const cards = document.querySelectorAll(".entertainment-card");
+const noResults = document.getElementById("noResults");
+
+searchInput.addEventListener("keyup", function () {
+    let filter = searchInput.value.toLowerCase();
+    let found = false;
+
+    cards.forEach(card => {
+        let title = card.querySelector("h3").textContent.toLowerCase();
+
+        if (title.includes(filter)) {
+            card.style.display = "block";
+            found = true;
+        } else {
+            card.style.display = "none";
+        }
+    });
+    if (!found) {
+        noResults.style.display = "block";
+    } else {
+        noResults.style.display = "none";
+    }
+});
